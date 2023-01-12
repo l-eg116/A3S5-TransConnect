cda4b048157ecda577f88d369c4ff0fe08c21494
l_eg <legcom6@gmail.com>
2023-01-12 09:10:23 +0100

    Added Payed attribute to Ticket

--- Ticket.cs
+++ Ticket.cs
@@ -10,11 +10,12 @@ namespace A3S5_TransConnect
 		public DateTime Date { get; set; }
 		private double _cost;
 		public double Cost { get => this._cost; set => this._cost = Math.Max(0, value); }
+		public bool Payed { get; set; }
 
-		public Ticket() : this(default, default, default, default, default, default, default)
+		public Ticket() : this(default, default, default, default, default, default, default, default)
 		{ }
 		public Ticket(Client? client = null, string origin = "Unknown", string destination = "Unknown",
-			Employee? driver = null, Vehicle? vehicle = null, DateTime? date = null, double cost = 0)
+			Employee? driver = null, Vehicle? vehicle = null, DateTime? date = null, double cost = 0, bool payed = false)
 		{
 			this.Client = client is null ? new Client() : client;
 			this.Origin = origin;
@@ -23,11 +24,12 @@ namespace A3S5_TransConnect
 			this.Vehicle = vehicle is null ? new Vehicle() : vehicle;
 			this.Date = date is null ? new DateTime() : (DateTime)date;
 			this.Cost = cost;
+			this.Payed = payed;
 		}
 
 		public override string ToString()
 			=> $"Ticket | Client: #{this.Client.SocialSecurityNumber}, {this.Origin} -> {this.Destination}, " +
 				$"Driver: #{this.Driver.SocialSecurityNumber}, Vehicle: #{this.Vehicle.NumberPlate}, Date: {this.Date}, " +
-				$"Cost: {this.Cost}€";
+				$"Cost: {this.Cost}€, Payed: {this.Payed}";
 	}
 }
