ae01ec4e7c07ddfa6e270e2fbe3ad3603b3e5e9c
l_eg <legcom6@gmail.com>
2023-01-21 17:30:29 +0100

    Added Company property to Client

--- Client.cs
+++ Client.cs
@@ -2,6 +2,7 @@
 {
 	class Client : Person, ITicketLinkable
 	{
+		public string Company { get; set; }
 		private HashSet<Ticket> _linkedTickets;
 		public HashSet<Ticket> LinkedTickets
 		{
@@ -12,19 +13,20 @@
 				value.ToList().ForEach(tkt => tkt.Client = this);
 			}
 		}
-		public Client() : this(default, default, default, default, default, default, default, default)
+		public Client() : this(default, default, default, default, default, default, default, default, default)
 		{ }
 		public Client(string firstName = "Unknown", string lastName = "UNKNOWN",
 			int socialSecurityNumber = 0, DateTime? birthday = null, string address = "",
-			string email = "", string phoneNumber = "", HashSet<Ticket>? linkedTickets = null)
+			string email = "", string phoneNumber = "", string company = "", HashSet<Ticket>? linkedTickets = null)
 			: base(firstName, lastName, socialSecurityNumber, birthday, address, email, phoneNumber)
 		{
+			this.Company = company;
 			this.LinkedTickets = linkedTickets ?? new HashSet<Ticket>();
 		}
 
 		public override string ToString()
 		{
-			return base.ToString() + $", {this.LinkedTickets.Count} LinkedTicket(s)";
+			return base.ToString() + $", Company : {this.Company}, {this.LinkedTickets.Count} LinkedTicket(s)";
 		}
 
 		public void AddLinkedTicket(Ticket tkt)
@@ -32,6 +34,10 @@
 		public void RemoveLinkedTicket(Ticket tkt)
 			=> tkt.Client = null;
 
-		public override List<PropertyCapsule> PropertyCapsules => base.PropertyCapsules;
+		public override List<PropertyCapsule> PropertyCapsules
+			=> base.PropertyCapsules.Concat(new List<PropertyCapsule>() {
+				new PropertyCapsule("Company : ", () => this.Company, () => this.Company = "",
+					l => this.Company = Display.CleanRead<string>("Company : ", l))
+			}).ToList();
 	}
 }
